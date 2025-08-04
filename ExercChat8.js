4b1b950661d214d768a359c8dc22cddaff766bf0
Davi Aguiar <davi.aguiarms@gmail.com>
2025-08-04 11:20:26 -0300

    Avançados

--- ExercChat8.js
+++ ExercChat8.js
@@ -1,17 +1,38 @@
-let nomeProduto = ['Arroz', 'feijao', 'Milho', 'Batata']
+const nomeCompradores = []
 
-let quantidadeEstoque = [0, 3, 7, 0]
+const idadeCompradores = []
 
-for (let i = 0; i < nomeProduto.length; i++) {
-    if (quantidadeEstoque[i] == 0) {
-        console.log(`Produto ${nomeProduto[i]} esta ESGOTADO`);
-    } else if (quantidadeEstoque[i] < 5) {
-        console.log(`Produto ${nomeProduto[i]} esta com BAIXO estoque (${quantidadeEstoque[i]} unidades)`);
+const pagouIngresso = []
 
-    } else if (quantidadeEstoque[i] >= 5) {
-        console.log(`Produto ${nomeProduto[i]} esta com estoque ok (${quantidadeEstoque[i]} unidades)`);
+let newComprador = prompt("Digite o nome do novo comprador (Digite não para parar)")
+
+let newIdade = prompt("Digite a idade do novo comprador")
+
+let newPagamento = prompt("Digite se o novo comprador pagou ingresso (sim = true e nao = false)")
+
+while (newComprador !== 'não') {
+    nomeCompradores.push(newComprador)
+
+    idadeCompradores.push(Number(newIdade))
+
+    pagouIngresso.push(newPagamento === 'true')
+
+    let newComprador = prompt("Digite o nome do novo comprador (Digite não para parar)")
+    if (newComprador === 'não') break;
+
+    let newIdade = prompt("Digite a idade do novo comprador")
+    let newPagamento = prompt("Digite se o novo comprador pagou ingresso (sim = true e nao = false)")
+}
+
+for (let i = 0; i < nomeCompradores.length; i++) {
+    if (pagouIngresso[i]) {
+        console.log(`${nomeCompradores[i]} acesso liberado`);
+
+    } else if (idadeCompradores[i] < 18) {
+        console.log(`${nomeCompradores[i]} cadastro invalido`);
+
+    } else {
+        console.log(`${nomeCompradores[i]} precisa regularizar o pagamento`);
 
     }
-
-
 }
