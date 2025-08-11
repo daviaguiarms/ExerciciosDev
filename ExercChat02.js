8701b9e5b92bfa644a69348885f0d0f641a9bb9a
Davi Aguiar <davi.aguiarms@gmail.com>
2025-08-11 00:54:10 -0300

    Mais exercs

--- ExercChat02.js
+++ ExercChat02.js
@@ -1,11 +1,39 @@
-const pontos = 55
+const livro = ['Hobbit', 'SpiderMan', 'Verity', 'Outside', 'it', 'Secret']
+const disponivel = [true, true, false, true, false, false]
+const nmrEmprestimo = [4, 6, 3, 2, 9, 5]
 
-const vidas = 1
+const emprestarLivro = (indice) => {
+    if (disponivel[indice]) {
+        disponivel[indice] = false
+        nmrEmprestimo[indice] += 1;
+    } else {
+        console.log(`livro ${livro[indice]} ja esta emprestado`);
 
-if (pontos > 50 && vidas >= 1) {
-    console.log('Proximo nivel liberado');
+    }
+}
 
+const devolverLivro = (indice) => {
+    if (!disponivel[indice]) {
+        disponivel[indice] = true
+        console.log(`${livro[indice]} devolvido com sucesso`);
+
+    }
+}
+
+let question = prompt('Se voce quer pegar um livro emprestado digite pegar, e se deseja devolver digite devolver')
+let escolhaLivro = prompt('Escolha qual livro quer pegar (0 a 5)')
+
+let indiceLivro = Number(escolhaLivro)
+if (question === 'pegar') {
+    emprestarLivro(indiceLivro);
+} else if (question === 'devolver') {
+    devolverLivro(indiceLivro);
 } else {
-    console.log('Nao foi possivel avançar o nivel');
+    console.log(`Opção invalida`);
+
+}
+
+for (let x = 0; x < livro.length; x++) {
+    console.log(` ${livro[x]} - Disponivel: ${disponivel[x]} - Numero de emprestimos: ${nmrEmprestimo[x]}`);
 
 }
