3836930c6a1d98d855dff22ad99c7fe44995a314
Davi Aguiar <davi.aguiarms@gmail.com>
2025-08-19 16:37:50 -0300

    Atualizando

--- ExercChat11.js
+++ ExercChat11.js
@@ -1,24 +1,52 @@
-const eleitores = ['Davi', 'Laura', 'Brener', 'Heitor', 'Irias', 'Joao']
+const products = [
+    { produto: 'Oleo', preco: 15, qtd: 2 },
+    { produto: 'Gas', preco: 180, qtd: 1 },
+    { produto: 'Arroz', preco: 20, qtd: 4 },
+    { produto: 'Feijao', preco: 30, qtd: 3 },
+    { produto: 'Milho', preco: 10, qtd: 5 },
+    { produto: 'Maçã', preco: 5, qtd: 3 }
+]
 
-const ageEleitores = [20, 19, 13, 15, 19, 16]
-
-const temTitulo = [true, true, false, false, false, true]
-
-const votou = [true, false, false, false, false, true]
-
-for (let i = 0; i < eleitores.length; i++) {
-    if (ageEleitores[i] < 16) {
-        console.log(`${eleitores[i]} deve ter mais de 16 anos para votar`);
-
-    } else if (!temTitulo[i]) {
-        console.log(`${eleitores[i]} deve ter titulo de eleitor para votar`);
-
-    } else if (votou[i]) {
-        console.log(`${eleitores[i]} ja fez o seu voto`);
-
-    } else {
-        console.log(`${eleitores[i]} pode votar`);
-
-    }
+const addItem = (item, custo, qtdProduct) => {
+    products.push({ produto: item, preco: custo, qtd: qtdProduct });
+    console.log(`${item} adicionado com sucesso`);
 
 }
+
+
+const removeItem = (produto) => {
+    let index = products.findIndex(p => p.produto === produto)
+    if (index !== -1) {
+        console.log(`${products[index].produto} removido com sucesso`);
+        products.splice(index, 1) 
+    } else {
+        console.log('Produto não encontrado');
+    }
+}
+
+let total = 0
+const valorTotal = () => {
+    total = products.reduce((acumul, adc) => acumul + (adc.preco * adc.qtd), 0);
+    console.log(`O valor total da compra é: ${total.toFixed(2)} reais`);
+
+}
+
+const desconto = () => {
+    if (total > 200) {
+        total -= total * 0.10
+        console.log('Desconto de 10% adicionado');
+        console.log(`O valor da compra agora é ${total}`);
+
+
+    } else {
+        console.log('O valor da conta é mt baixo para ter desconto');
+
+    }
+}
+
+
+addItem('Pao', 8, 3);
+removeItem('Milho');
+valorTotal();
+desconto();
+console.log(products);
--- ExercChat6.js
+++ ExercChat6.js
@@ -1,25 +1,8 @@
-const alunos = ['Davi', 'Laura', 'Brener', 'Heitor', 'Irias', 'Joao']
-
-const ageAlunos = [20, 19, 13, 15, 19, 12]
-
-const mensalidadePaga = [true, false, false, true, true, false]
-
-const exameMedico = [true, true, false, true, false, false]
-
-const acompanhante = [false, false, true, false, false, true]
-
-for (let i = 0; i < alunos.length; i++) {
-    if (!mensalidadePaga[i]) {
-        console.log(`O aluno ${alunos[i]} deve pagar a mensalidade para entrar`);
-
-    } else if (!exameMedico[i]) {
-        console.log(`O aluno ${alunos[i]} deve ter exame medico valido para entrar`);
-
-    } else if (ageAlunos[i] < 16 && !acompanhante[i]) {
-        console.log(`O aluno ${alunos[i]} deve estar acompanhado para entrar `);
-
-    } else {
-        console.log(`${alunos[i]} pode entrar`);
-
-    }
-}
+const products = [
+    { produto: 'Oleo', preco: 15, qtd: 2 },
+    { produto: 'Gas', preco: 180, qtd: 1 },
+    { produto: 'Arroz', preco: 20, qtd: 4 },
+    { produto: 'Feijao', preco: 30, qtd: 3 },
+    { produto: 'Milho', preco: 10, qtd: 5 },
+    { produto: 'Maçã', preco: 5, qtd: 3 }
+] 
